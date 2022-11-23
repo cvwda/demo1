fe44c4266981a05fc5575e8922f7f83d4cd7e286
lwei <lwei@qq.com>
2022-11-23 21:38:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author li
+ * @create @create 2022-11-23 21:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
